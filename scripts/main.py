6045eed99fbfe0ef34286baadbaa8deef28a526c
Eileen Luo <eluo.5230@gmail.com>
2025-02-10 09:47:00 -0600

    Fleshing out pseudocode

--- scripts/main.py
+++ scripts/main.py
@@ -5,11 +5,13 @@ def main():
     # Inputs
     n = 200 # Upper limit of variants 
     primary_diseases = [[]] # Nested list; a list of keywords/IDs for each disease of interest
+    gwas_api = 'opengwas' # Options: 'opengwas', 'nih'
 
     # Call the pipeline
-    comorbid_variants = get_comorbid_variants(primary_diseases, n)
-
-
+    # Output: list of significant GVs for every disease of interest 
+    #NOTE: the GWAS API is currently limited to SNPs
+    cmb_csv = pd.read_csv('input/comorbidities copy.csv')
+    comorbid_variants = get_comorbid_variants(cmb_csv, gwas_api, n)
 
 
 main()
--- scripts/pre_analysis.py
+++ scripts/pre_analysis.py
@@ -1,5 +1,5 @@
-from pandasgwas import get_studies
-
+from pandasgwas.get_variants import get_variants_by_reported_trait
+import pandas as pd
 
 '''
 *** STEP 2: DETERMINE INFLUENTIAL COMORBID GENES ***
@@ -9,11 +9,32 @@ These genes should be influential in both their relevant comorbidity and the pri
 
 # Main function to 
 # n: Upper threshold for number of genetic variants
-# primary_diseases: Nested list; a list of keywords/IDs for each disease of interest
-def get_comorbid_variants(primary_diseases = [[]], n = 100):
-    # For each primary_disease
-        # Get comorbidities
-        # Get: gene name, association strength, association direction -> pd_variants
+def get_comorbid_variants(cmb_csv, api, n = 100):
+    # Read in comorbidities for each variant
+    cmb_csv.drop(list(cmb_csv.filter(regex = 'Prevalence')), axis=1, inplace=True)
+    primary_diseases = cmb_csv.columns # List of each disease of interest
+    # TODO: remove code block below. Kept during initial development phase.
+    banned = ['males', 'females']
+    remove_words = lambda x: ' '.join([item for item in x.split() if item not in banned])
+    cmb_csv = {col: cmb_csv[col].apply(remove_words) for col in cmb_csv.columns}
+    #
+    # Create dictionary to store variants (SNPs only for now)
+    variants = {key for key in pd.unique(cmb_csv).values.ravel('K')}
+    variants = variants | {key: [] for key in primary_diseases}
+
+    # Get variants as gene names
+    for v in variants.keys:
+        variants[v] = call_gwas_api(api) 
+
+    tmp = variants[0] #TODO: through which VariantObject attribute(s) can we get the genetic variants?
+    variants_df = pd.DataFrame(variants)
+    variants_df.map(lambda x: x)
+
+    # Intersect with respective primary disease
+    significant_variants = pd.DataFrame.from_dict(columns=cmb_csv.columns)
+
+    for pd in significant_variants.columns:
+        significant_variants = variants[pd]
     
     # For each comorbidity (within each primary_disease)
         # search the GWAS catalog for keywords.
@@ -21,3 +42,14 @@ def get_comorbid_variants(primary_diseases = [[]], n = 100):
         # Result: List of ALL significant genetic variants for a primary_disease -> cmb_variants
         # Intersect cmb_variants, pd_variants
 
+
+    #TODO: how will you deal with comorbidities only influential in males? or certain ethnicities?
+    #
+
+
+# Wrapper function to call different APIs
+def call_gwas_api(option):
+    if option == 'opengwas':
+        return get_variants_by_reported_trait(v)
+    elif option == 'nih':
+        return []
